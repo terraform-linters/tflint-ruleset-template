c5c142205b5943fff8d0fbc6570e117e1a7bcf86
wata_mac <watassbass@gmail.com>
2022-03-21 16:19:23 +0900

    Bump tflint-plugin-sdk to v0.10.0

--- rules/local_file_example_provisioner.go
+++ rules/local_file_example_provisioner.go
@@ -1,13 +1,14 @@
 package rules
 
 import (
-	hcl "github.com/hashicorp/hcl/v2"
-	"github.com/terraform-linters/tflint-plugin-sdk/terraform/configs"
+	"github.com/terraform-linters/tflint-plugin-sdk/hclext"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
 // LocalFileExampleProvisionerRule checks whether ...
-type LocalFileExampleProvisionerRule struct{}
+type LocalFileExampleProvisionerRule struct {
+	tflint.DefaultRule
+}
 
 // NewLocalFileExampleProvisionerRule returns a new rule
 func NewLocalFileExampleProvisionerRule() *LocalFileExampleProvisionerRule {
@@ -25,7 +26,7 @@ func (r *LocalFileExampleProvisionerRule) Enabled() bool {
 }
 
 // Severity returns the rule severity
-func (r *LocalFileExampleProvisionerRule) Severity() string {
+func (r *LocalFileExampleProvisionerRule) Severity() tflint.Severity {
 	return tflint.ERROR
 }
 
@@ -36,29 +37,36 @@ func (r *LocalFileExampleProvisionerRule) Link() string {
 
 // Check checks whether ...
 func (r *LocalFileExampleProvisionerRule) Check(runner tflint.Runner) error {
-	return runner.WalkResources("local_file", func(resource *configs.Resource) error {
+	resources, err := runner.GetResourceContent("local_file", &hclext.BodySchema{
+		Blocks: []hclext.BlockSchema{
+			{
+				Type:       "provisioner",
+				LabelNames: []string{"name"},
+				Body: &hclext.BodySchema{
+					Attributes: []hclext.AttributeSchema{
+						{Name: "command"},
+					},
+				},
+			},
+		},
+	}, nil)
+	if err != nil {
+		return err
+	}
 
-		for _, provisioner := range resource.Managed.Provisioners {
-			if provisioner.Type != "local-exec" {
+	for _, resource := range resources.Blocks {
+		for _, provisioner := range resource.Body.Blocks {
+			if provisioner.Labels[0] != "local-exec" {
 				continue
 			}
 
-			content, _, diags := provisioner.Config.PartialContent(&hcl.BodySchema{
-				Attributes: []hcl.AttributeSchema{
-					{Name: "command"},
-				},
-			})
-			if diags.HasErrors() {
-				return diags
-			}
-
-			if attr, exists := content.Attributes["command"]; exists {
-				if err := runner.EmitIssueOnExpr(r, "local-exec provisioner command found", attr.Expr); err != nil {
+			if attr, exists := provisioner.Body.Attributes["command"]; exists {
+				if err := runner.EmitIssue(r, "local-exec provisioner command found", attr.Expr.Range()); err != nil {
 					return err
 				}
 			}
 		}
+	}
 
-		return nil
-	})
+	return nil
 }
